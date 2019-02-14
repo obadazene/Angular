ab1e33b378f2ce8a7994798745f00b2eae9e032e
obadazene <obada.zene@hotmail.com>
2019-02-14 10:39:36 +0100

    ah ah ah

--- coetusIVFront/src/app/app.module.ts
+++ coetusIVFront/src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PersonaComponent } from './persona/persona.component';
+import { AppRoutingModule } from './app.routing.module'
 import {PersonaService} from './persona.service';
-
-
+import {HttpClientModule} from "@angular/common/http";
+import {AddPersonaComponent} from  './persona/add-persona.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PersonaComponent
+    PersonaComponent,
+    AddPersonaComponent
     
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
     
   ],
   providers: [PersonaService],
